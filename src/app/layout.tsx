6ddc6c4037d9216fed4d138a3609e28f51a2a16c
Gabriel Pérez dIEZ <gaabgames@gmail.com>
2024-10-28 10:51:08 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import ClientProvider from "@/components/ClientProvider";
 import { UserProvider } from "@/context/UserContext";
 import type { Metadata } from "next";
+import dynamic from "next/dynamic";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -9,6 +9,10 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Bastibus",
 };
+
+const ClientProvider = dynamic(() => import("@/components/ClientProvider"), {
+  ssr: false, // disables server-side rendering for this component
+});
 
 export default function RootLayout({
   children,
